7c666368dd8a32e81c84aa45f963a262b72a4062
CarlaOnate <carlaonategardella@gmail.com>
2022-03-29 14:48:28 -0600

    Added urls file, db setting file

--- Drummys/Drummys/urls.py
+++ Drummys/Drummys/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('unity/', include('game.urls'))
+    path('', include('unity.urls'))
 ]
--- Drummys/unity/views.py
+++ Drummys/unity/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+def index(req):
+    return HttpResponse('<p>Soy Index</p>')
