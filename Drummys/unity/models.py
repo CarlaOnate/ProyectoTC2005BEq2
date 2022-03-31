f549ed72fd1fe3bfd2d1a752924af0cee30be069
CarlaOnate <carlaonategardella@gmail.com>
2022-03-30 23:14:36 -0600

    Saving model progress, so far works but I'm not seeing the values created in the DB

--- Drummys/unity/models.py
+++ Drummys/unity/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Countries(models.Model):
+    # Campos
+    name = models.CharField(max_length=20)
+    nickname = models.CharField(max_length=20)
+
+class User(models.Model):
+    # Campos
+    username = models.CharField(max_length=20)
+    age = models.IntegerField()
+    password = models.CharField(max_length=20)
+    country_id = models.ForeignKey(Countries, on_delete=models.PROTECT)
+
+
+
--- Drummys/unity/views.py
+++ Drummys/unity/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import User, Countries
 
 # Create your views here.
+
+
 def index(req):
     return HttpResponse('<p>Soy Index</p>')
+
+@csrf_exempt
+def modelo(req):
+    username = req.POST["username"]
+    age = req.POST["age"]
+    password = req.POST["password"]
+    country = req.POST["country"]
+
+    country = Countries.objects.get(nickname="MX")
+
+    print('\n\ncountry_id =>', country.nickname, '\n\n')
+
+    user = User(username=username, age=age, password=password, country_id=country)
+
+    print('\n\nuser', user, '\n\n')
+
+    user.save()
+
+    return JsonResponse({"msg": 200})
