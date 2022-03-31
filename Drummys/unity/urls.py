4b3f3de64838dd06cbb6c7805b0db938422c18f5
CarlaOnate <carlaonategardella@gmail.com>
2022-03-30 22:03:42 -0600

    signup endpoint - not saving in DB

--- Drummys/unity/urls.py
+++ Drummys/unity/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    #POST
+    path('user/signup', views.signup, name='signup')
 ]
--- Drummys/unity/views.py
+++ Drummys/unity/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import sqlite3
 
 # Create your views here.
 def index(req):
     return HttpResponse('<p>Soy Index</p>')
+
+@csrf_exempt
+def signup(req):
+    username = req.POST["username"]
+    age = req.POST["age"]
+    password = req.POST["password"]
+    country = req.POST["country"]
+
+    mydb = sqlite3.connect("DrummyDB.db")
+    cur = mydb.cursor()
+    stringSQL = '''INSERT INTO User (username, country_id, password, age) VALUES (?, ?, ?, ?)'''
+    # cur.execute(stringSQL, (username, age, password, country,))
+    cur.execute(stringSQL)
+
+    return JsonResponse({"msg": 200})
