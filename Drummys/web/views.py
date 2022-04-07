ac8d3d593b92d832f7128dd7538af71bcfcc3ee5
Victoria Vazquez <65380353+VickyA01654095@users.noreply.github.com>
2022-04-06 19:50:49 -0500

    Top scores table template

--- Drummys/web/views.py
+++ Drummys/web/views.py
@@ -1,6 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from json import loads,dumps
+from django.http import Http404
+import datetime
+import collections
+import sqlite3
 
 # Create your views here.
 def index(req):
     return render(req, 'web/index.html', ({"valor": 111}))
+
+
+def topscores_global(request):
+    mydb = sqlite3.connect("DrummyDB.db")
+    cur = mydb.cursor()
+    stringSQL = '''SELECT Party.id, User.id as User_ID, User.username, Countries.nickname as Country, 
+Party.total_score, Party.time_played, Party.dateCreated FROM  Party
+ INNER JOIN User, Countries ON Party.user_id = User.id  AND Countries.id = User.country_id ORDER BY Party.total_score LIMIT 10 '''
+    rows = cur.execute(stringSQL)
+    if rows is None:
+        raise Http404("user_id does not exist")
+    else:
+        lista_salida = [["Username", "Country", "Total Score", "Time Played", "Date"]]
+        for r in rows:
+            print(r)
+            d = [r[2], r[3], r[4], r[5], r[6]]
+            lista_salida.append(d)
+        j = dumps(lista_salida)
+
+    title = 'Global Top Scores'
+    modified_title = dumps(title)
+
+    return render(request, 'tablaTopScores.html', {'values': j, 'title': modified_title})
+
